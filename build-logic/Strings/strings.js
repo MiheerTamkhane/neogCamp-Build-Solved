a95979e6c65f24a8b10a9764437c80a4facdd7ef
Miheer Vilas Tamkhane <sarangtamkhane10@gmail.com>
2021-09-30 19:50:18 +0530

    started strings

---
+++ build-logic/Strings/strings.js
@@ -0,0 +1,31 @@
+// Write a program that converts the string into uppercase
+// var str = "neogCamp";
+// console.log(str.toLocaleUpperCase());
+
+// // Write a program that reads two strings and append first string to the second. So if first string is Good second string is Morning , the program should print MorningGood
+// var str = "Good";
+// var str2 = "morning" + str;
+// console.log(str2);
+
+// Program that reads string and count number of characters present in the string
+
+// var str = "neoGcamp";
+// console.log(str.length);
+
+// Write a program that converts string like "124" to 124
+// var str = "124";
+// console.log(str.toString());
+
+// Write a program to delete all vowels from a string. Assume string is not more than 80 characters long
+// var str = "neoGcamp";
+// var string = str.split("");
+// if (str.length < 80) {
+//   let newStr = string.map((item) => {
+//     return item.replace(/[aeiou]/g, "");
+//   });
+//   console.log(newStr.join(""));
+// } else {
+//   console.log("String must have only 80 chars");
+// }
+
+// Write a program to check whether the string is alphanumeric or not , eg:batman@45 contains digit 45
